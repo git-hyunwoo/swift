8c9df12f98dc93d807f73a84de723b0a0a1d72ca
dev choi <choiman2021@gmail.com>
2025-04-04 23:57:29 +0900

    simpler way to include values in strings

--- simpleValues.swift
+++ simpleValues.swift
@@ -24,3 +24,11 @@ Values are never implicitly converted to another type. If you need to convert a 
 let label = "The width is "
 let width = 94
 let widthLabel = label + String(width)
+
+/*
+There’s an even simpler way to include values in strings: Write the value in parentheses, and write a backslash (\) before the parentheses. For example
+*/
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
