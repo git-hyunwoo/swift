833d720a5385ba05c994f0911544e8cc0e939073
dev choi <choiman2021@gmail.com>
2025-04-05 01:37:02 +0900

    Use three double quotation marks

--- simpleValues.swift
+++ simpleValues.swift
@@ -32,3 +32,15 @@ let apples = 3
 let oranges = 5
 let appleSummary = "I have \(apples) apples."
 let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+/*
+Use three double quotation marks (""") for strings that take up multiple lines. Indentation at the start of each quoted line is removed, as long as it matches the indentation of the closing quotation marks. For example:
+*/
+let quotation = """
+        Even though there's whitespace to the left,
+        the actual lines aren't indented.
+            Except for this line.
+        Double quotes (") can appear without being escaped.
+
+        I still have \(apples + oranges) pieces of fruit.
+        """
